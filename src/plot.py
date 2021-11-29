9fe6769eb4e2c5a0dddb5f397526fbb16bd7b9d8
Siddharth Reed <slreed@andrew.cmu.edu>
2021-11-29 02:39:56 -0500

    initial idea for a cool figure

---
+++ src/plot.py
@@ -0,0 +1,12 @@
+"""
+Plotting utilities to generate figures
+
+Plot idea:
+ - plot gif of k_ac (decreasing sparsity) and model dynamics (1 k_ac per frame
+ - left half is heatmap of K_ac matrix (0=white, 1=color)
+ - right half is entire simulation of the model (all time steps for that K_ac)
+ - x axis is time, y axis could be
+   - model dynamics (just strain abundances as lines)
+   - model properties (alpha/beta diversity, growth rate, etc.)
+"""
+# import seaborn as sns
