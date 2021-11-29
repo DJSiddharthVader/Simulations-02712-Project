67b20288184be076c8363c771d10b1fd8d98378c
Siddharth Reed <slreed@andrew.cmu.edu>
2021-11-29 14:58:04 -0500

    cleaned up plotting function for the simulation

--- src/plot.py
+++ src/plot.py
@@ -9,4 +9,36 @@ Plot idea:
    - model dynamics (just strain abundances as lines)
    - model properties (alpha/beta diversity, growth rate, etc.)
 """
+import matplotlib.pyplot as plt
 # import seaborn as sns
+
+
+def plot_model(results):
+    t, N, S, E, P = results
+    # Set up
+    fig, ax = plt.subplots(2, 2, sharex=True)
+    axN, axS, axE, axP = ax[0][0], ax[0][1], ax[1][0], ax[1][1]
+    fig.suptitle("Model Simulation")
+    # Plot Strains
+    labels = ["N{}".format(i) for i in range(1, len(N)+1)]
+    axN.plot(t, N.T, label=labels)
+    axN.set_title("Strains")
+    axN.legend()
+    # Plot Signals
+    labels = ["S{}".format(i) for i in range(1, len(S)+1)]
+    axS.plot(t, S.T, label=labels)
+    axS.set_title("Signals")
+    axS.legend()
+    # Plot Exo-Enzyme
+    axE.plot(t, E)
+    axE.set_title("Exo-Enzyme")
+    # Plot Usable Nutrient
+    axP.plot(t, P)
+    axP.set_title("Usable Nutrient")
+    # Formatting
+    fig.tight_layout()
+    fig.add_subplot(111, frameon=False)
+    plt.tick_params(labelcolor='none', which='both',
+                    top=False, bottom=False, left=False, right=False)
+    plt.xlabel("Time")
+    plt.show()
