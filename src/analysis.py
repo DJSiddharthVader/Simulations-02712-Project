d1685908736c57fc49d9619896907ab3c676cbef
Siddharth Reed <slreed@andrew.cmu.edu>
2021-11-30 22:07:41 -0500

    added some wrappers for computing the stats on the model outputs

--- src/analysis.py
+++ src/analysis.py
@@ -15,6 +15,17 @@ The diversity measures are taken from @calle_2019
 """
 
 
+def compute_stats(N):
+    """compute_stats.
+
+    :param N:
+    """
+    stats = {}
+    stats["growth_rate"] = growth_rate(N.T)
+    N = np.apply_along_axis(normalize, 1, N.T).T
+    return stats
+
+
 def normalize(abundance):
     """normalize.
     Normalize the abundance vector.
@@ -25,19 +36,69 @@ def normalize(abundance):
     return [x/div for x in abundance]
 
 
+# Statistics computed from all time steps
 def growth_rate(timesteps):
     """growth_rate.
     Growth rate of the total population size.
     Population size is not constant in the model
     so can be useful to examine how it grows.
 
-    :param timesteps: abundance vector at each time point
+    :param timesteps: abundance vector at each time step
     """
     p1, pn = sum(timesteps[0]), sum(timesteps[-1])
     return (pn-p1)/(p1*len(timesteps))
 
 
-def skewness(abundance):
+def has_grown(timesteps):
+    """has_grown.
+    Proportion of strains that have increased in
+    abundance by the end of the simulation
+
+    :param timesteps: abudance vector at each time step
+    """
+    t1s, tns = timesteps[0], timesteps[-1]
+    return sum([1 for t1, tn in zip(t1s, tns) if tn > t1])/len(t1s)
+
+
+def skewness(timesteps):
+    """skewness.
+    Calculate Fisher-Pearson coefficient of
+    skewness for the abundance i.e. how far
+    from normal are the abundaces distributed.
+
+    :param timesteps: abudance vector at each time step
+    """
+    t1, tn = skewness_(timesteps[0]), skewness_(timesteps[-1])
+    return tn - t1
+
+
+def shannon_index(timesteps):
+    """shannon_index.
+    Calculate the shannon index of a population,
+    this is a type of alpha diversity.
+
+    :param timesteps: abudance vector at each time step
+    """
+    t1, tn = shannon_index_(timesteps[0]), shannon_index_(timesteps[-1])
+    return tn - t1
+
+
+def bray_curtis(timesteps):
+    """bray_curtis.
+    Calculate the bray curtis distance between 2
+    abundnace vectors. Assumes that both vectors
+    contain the same OTU set in the same order
+    (entries can be zero). This is a type of
+    beta diversity.
+
+    :param timesteps: abudance vector at each time step
+    """
+    t1, tn = timesteps[0], timesteps[-1]
+    return bray_curtis_(tn, t1)
+
+
+# Statistics computed on a single abundance vector
+def skewness_(abundance):
     """skewness_.
     Calculate Fisher-Pearson coefficient of
     skewness for the abundance i.e. how far
@@ -48,17 +109,7 @@ def skewness(abundance):
     return skew(abundance)
 
 
-def richness(abundance):
-    """richness.
-    Calculate species richness of a sample,
-    this is a type of alpha diversity.
-
-    :param abundance: bacterial abundance vector
-    """
-    return sum([1 for x in abundance if x > 0])/len(abundance)
-
-
-def shannon_index(abundance):
+def shannon_index_(abundance):
     """shannon_index.
     Calculate the shannon index of a population,
     this is a type of alpha diversity.
@@ -68,7 +119,7 @@ def shannon_index(abundance):
     return -sum([p_i*np.log(p_i) for p_i in abundance])
 
 
-def bray_curtis(abundance1, abundance2):
+def bray_curtis_(abundance1, abundance2):
     """bray_curtis.
     Calculate the bray curtis distance between 2
     abundnace vectors. Assumes that both vectors
