a45a125b395b9b53d45068836a77dc9b0b6e5404
Siddharth Reed <slreed@andrew.cmu.edu>
2021-11-30 22:08:35 -0500

    writing function to produce dataframe of different simulation results for plotting

--- src/sparsity_analysis.py
+++ src/sparsity_analysis.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matrix
 import model
 import analysis as an
+# from tqdm import tqdm
 
 
 """
@@ -12,8 +13,49 @@ by running analysis() and I will create plots with code in plot.py
 """
 
 
-def analysis(init, sparsity, end_time):
-    """sparsity_analysis.
+def analyze(size, num, lower, upper, sparsity, end_time):
+    """analyze.
+    Preform the sparsity analysis using different
+    random initial populations for different
+    populatin sizes between 2 and size.
+
+    :param size: size of each population
+    :param num: number of initial populations
+    :param lower: minimum abundance
+    :param upper: maximum abundance
+    :param sparsity: maximum sparsity value to try
+    :param end_time: time steps to simulate
+    """
+    pop_dfs = []
+    for pop_size in range(2, size+1):
+        inits = an.make_random_abundances(pop_size, num, lower, upper)
+        idfs = []
+        for p_i, init in enumerate(inits):
+            dfs = [analyze_run(init, sparsity, end_time)
+                   for init in inits]
+            dfs = [append_static_col(df, p_i, 'Init') for df in dfs]
+            idfs += [pd.concat(dfs).reset_index(drop=True)]
+        idf = pd.concat(idfs).reset_index(drop=True)
+        idf = append_static_col(idf, pop_size, 'Size')
+        pop_dfs += [idf]
+    return pd.concat(pop_dfs)
+
+
+def append_static_col(df, value, name):
+    """append_static_col.
+    Append a column with all with the
+    same value to a data frame
+
+    :param df: dataframe
+    :param value: column value
+    :param name: column name
+    """
+    col = pd.DataFrame({name: [value]*df.shape[0]})
+    return pd.concat([df, col], axis=1)
+
+
+def analyze_run(init, sparsity, end_time):
+    """analyze_run.
     Generate simulation data to analyze how K_ac
     sparsity affects model dynamics.
 
@@ -26,26 +68,7 @@ def analysis(init, sparsity, end_time):
         # simulate model with given params
         t, N, S, E, P = model.simulate(K_ac, init, end_time)
         # Calculate statistics
-        row = {"sparsity": np.sum(K_ac)/(n*n)}
-        # N, S are variables by time points matrices
-        row["growth_rate"] = an.growth_rate(N.T)
-        N = np.apply_along_axis(an.normalize, 1, N.T).T
-        # store for easy conversion to a dataframe
+        row = an.compute_stats(N)
+        row["sparsity"] = np.sum(K_ac)/(n*n)
         rows.append(row)
     return pd.DataFrame(rows)
-
-
-def skewness(timesteps):
-    return None
-
-
-def richness(timesteps):
-    return None
-
-
-def shannon_index(timesteps):
-    return None
-
-
-def bray_curtis(timesteps):
-    return None
